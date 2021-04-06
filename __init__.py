7447ab70adcbb4c1f01e6b447b97f245eed6198d
Christian R. Picone <christian.picone@geo-solutions.it>
2021-04-06 16:12:58 +0200

    added bundle to the geotiflib whl

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+try:
+    import geotiflib
+except ImportError:
+    import sys
+    import os
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(this_dir, 'lib', 'geotiflib-1.0.7-py3-none-any.whl')
+    sys.path.append(path)
+    import geotiflib
+
+
 def classFactory(iface):
     from .oawplugin import OAWPlugin
     return OAWPlugin(iface)
